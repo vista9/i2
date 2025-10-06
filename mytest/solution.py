e15f46c03555f5555dbcb3a1437d177b829d6482
vista9 <181370512+vista9@users.noreply.github.com>
2025-10-06 15:16:08 +0500

    практ. работа 1

--- mytest/solution.py
+++ mytest/solution.py
@@ -1,3 +1,16 @@
-def analyze_temperature(temperatures):
+def analyze_temperature(temperatures: list[float]) -> dict[str, float] | None:
+    if not temperatures or len(temperatures) != 7:
+        return None
+    
+    average_temp = sum(temperatures) / len(temperatures)
 
-if __name__ == "__main__":
+    return {
+        "average": round(average_temp, 1),
+        "max": max(temperatures),
+        "min": min(temperatures),
+        "hot_days": sum(1 for temp in temperatures if temp > 25),
+        "cold_days": sum(1 for temp in temperatures if temp < 10)
+    }
+
+# if __name__ == "__main__":
+    
--- mytest/solution2.py
+++ mytest/solution2.py
@@ -1,3 +1,17 @@
-def analyze_purchases(items, prices, discount_threshold=1000):
+def analyze_purchases(items: list[str], prices: list[float], discount_threshold: float = 1000) -> dict[str, str | float] | None:
+    if len(items) != len(prices) or not items or not prices or any(price < 0 for price in prices):
+        return None
 
-if __name__ == "__main__":
+    discount = 10 # 10%
+    total_cost = sum(prices)
+    final_total = total_cost * (1 - discount / 100) if total_cost >= discount_threshold else total_cost
+    
+    return {
+        "total": total_cost,
+        "average": round(total_cost / len(prices), 2),
+        "most_expensive": items[prices.index(max(prices))],
+        "discount_applied": total_cost >= discount_threshold,
+        "final_total": round(final_total, 2)
+    }
+
+# if __name__ == "__main__":
